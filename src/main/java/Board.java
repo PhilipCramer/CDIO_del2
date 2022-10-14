a11e0f29a0fec5a87c96e5eaf256e817303e9ab0
Philip Astrup Cramer <s224319@dtu.dk>
2022-10-14 11:48:19 +0200

    created method: getEffect() in Board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -3,7 +3,12 @@ public class Board {
 
     public Board(int NumberOfFields){
         for(int i =0; i < NumberOfFields; i++){
-            Board[i] = new Field(i+1);
+            Board[i] = new Field();
         }
+    }
+
+    public int getEffect(){
+        //returns the effect from the field
+        return 0;
     }
 }
