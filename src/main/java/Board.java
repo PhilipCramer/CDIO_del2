8dbc079d0d9f4b79a5ac3cc8750b1e8a4ed89a75
Philip Astrup Cramer <s224319@dtu.dk>
2022-10-14 12:05:04 +0200

    Created enumerator and documented constructor

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,9 +1,32 @@
 public class Board {
     Field[] Board;
 
-    public Board(int NumberOfFields){
-        for(int i =0; i < NumberOfFields; i++){
+    enum FieldNames{
+        START,
+        TOWER,
+        CRATER,
+        PALACE_GATES,
+        COLD_DESERT,
+        WALLED_CITY,
+        MONASTERY,
+        BLACK_CAVE,
+        HUTS_IN_THE_MOUNTAIN,
+        THE_WEREWALL,
+        THE_PIT,
+        THE_GOLDMINE
+    }
+
+    /**
+     *
+     * Constructs an array of fields based on the enumerator
+     * and sets the names correspondingly
+     */
+    public Board(){
+        int i = 0;
+        for(FieldNames fn : FieldNames.values()){
             Board[i] = new Field();
+            // board[i].setName(fn);
+            i++;
         }
     }
 
