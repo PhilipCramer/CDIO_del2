2b90299f58cbc8b146af25b807a213b4f151a89f
Philip Astrup Cramer <s224319@dtu.dk>
2022-10-15 16:01:04 +0200

    Board.getFieldtext() method added with documentation (nonfunctional)

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -17,7 +17,6 @@ public class Board {
     }
 
     /**
-     *
      * Constructs an array of fields based on the enumerator
      * and sets the names correspondingly
      */
@@ -30,8 +29,22 @@ public class Board {
         }
     }
 
+    /**
+     *
+     * @param DiceRollSum
+     * @return returns the integer effect to be further processed
+     */
     public int getEffect(int DiceRollSum){
         //returns the effect from the field
         return 0;
+    }
+
+    /**
+     *
+     * @param DiceRollSum enter the sum of the rolled diced.
+     * @return returns the action as text corresponding to the field
+     */
+    public String getFieldtext(int DiceRollSum){
+        return "";
     }
 }
