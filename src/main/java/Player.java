c6e81b829382d735cafeeeeba5ba3efd5786dd57
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2022-10-24 10:21:36 +0200

    Begyndt på player

---
+++ src/main/java/Player.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Player {
+
+    System.out.println("Please enter the player name");
+    Scanner playerName = new Scanner(System.in);
+    String name = " ";
+    player = new Player(name);
+
+
+    // playerName: string
+    // account : Account
+    // setPleyerName()
+    // getPlayerName(): String
+    // addValue (int value)
+}
