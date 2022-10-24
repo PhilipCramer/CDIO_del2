facc12268d8708c7766b9831eaae944aac2f276b
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2022-10-24 16:06:53 +0200

    Made the Die class

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,2 +1,16 @@
-public class Die {
+import java.util.Random;
+public class Die{
+    //Minimum value of the die can be changed if needed.
+    private int DieMin = 1;
+    //Maximum value of the die can be changed if needed.
+    private int DieMax = 6;
+
+    /**
+     * Generates the random number
+     * @return Random int in range from DieMin to DieMax
+     */
+    public int Throw(){
+        Random randomObj = new Random();
+        return randomObj.nextInt(DieMax) + DieMin;
+    }
 }
