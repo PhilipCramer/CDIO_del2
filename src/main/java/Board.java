e8292f765b46ed2539b945c7c7861c70d24d6e40
Philip Astrup Cramer <s224319@dtu.dk>
2022-10-24 08:42:42 +0200

    added code to Board.getFieldtext (requires Field.getName to be addded before being usable)

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -45,6 +45,21 @@ public class Board {
      * @return returns the action as text corresponding to the field
      */
     public String getFieldtext(int DiceRollSum){
-        return "";
+        String name = Board[DiceRollSum +1].getName().replace("_", " ").toLowerCase();
+        String str = "You have arrived at " + name;
+
+        //checks if the user gains or loses point and changes accordingly
+        if(getEffect(DiceRollSum) > 0){
+            str += " you lose " + Math.abs(getEffect(DiceRollSum)) + " points.";
+        } else{
+            str += " you gain " + Math.abs(getEffect(DiceRollSum)) + " points.";
+        }
+
+        // Adds the roll again text if user lands at the Werewall
+        if(DiceRollSum == 10){
+            str += " Roll again.";
+        }
+
+        return str;
     }
 }
