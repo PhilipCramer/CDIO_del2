b1547a0618d969f3a58dfd0d2c5e51118c2fa50c
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-10-25 16:21:41 +0200

    wrote TC1 & TC2 for account class

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AccountTest {
+    private Account account;
+
+    @BeforeClass
+    public void createAccount() {
+        account = new Account();
+        assertEquals(1000, account.balance);
+    }
+
+    @Test
+    public void TC1() {
+        account.addValue(-500);
+        assertEquals(500, account.balance);
+    }
+   
+    @Test
+    public void TC2(){
+        account.addValue(-1001);
+        assertEquals(0, account.balance);
+    }
+}
