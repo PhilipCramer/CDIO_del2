f8c7e21a82593f85bde5d1f916793c561bd22a8f
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-10-26 13:53:56 +0200

    minor changes in game class prints

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -21,14 +21,14 @@ public class Game {
         DieCup diceCup = new DieCup(DIE_COUNT);
 
         // welcome players, ask for name for player 1
-        System.out.println("Welcome!\nPlease enter the name of player one:");
+        System.out.print("Welcome!\nPlease enter the name of player one:\n");
         String player1 = sc.nextLine().trim();
 
         // initialize player1
         Player p1 = new Player(player1);
 
         // ask for name for player 2
-        System.out.println("Now enter the name of player two:");
+        System.out.print("\nNow enter the name of player two:\n");
         String player2 = sc.nextLine().trim();
 
         // initialize player2
@@ -40,11 +40,11 @@ public class Game {
         // game start
         while (true) {
             // ask player 1 to throw dice by pressing enter
-            System.out.println(currentPlayer.getPlayerName() + " press enter to throw dice");
+            System.out.print("\n" + currentPlayer.getPlayerName() + " press enter to throw dice");
             String start = sc.nextLine();
             while (!start.equals("")) {
                 // ask player to press enter
-                System.out.println("input not understood.\nPress enter to throw dice");
+                System.out.print("input not understood.\nPress enter to throw dice");
                 start = sc.nextLine();
                 continue;
             }
@@ -56,7 +56,7 @@ public class Game {
 
             // uncomment below if player should also know the individual dice and the total
             // dice value
-            // System.out.println("you got a " + diceCup.getDiceValue() + " and " +
+            // System.out.print("you got a " + diceCup.getDiceValue() + " and " +
             // diceCup.getDiceValue() + " = " + diceCup.getTotalValue());
 
             // tell the player where they landed
@@ -70,17 +70,16 @@ public class Game {
             if (isWinner) {
                 // insert nested if statement, if check for round equality is neccersary
                 if (currentPlayer == p1) {
-                    System.out.println(currentPlayer.getPlayerName()
+                    System.out.print(currentPlayer.getPlayerName()
                             + " congratulations you've hit the threshold with a balance of "
                             + currentPlayer.getPlayerBalance() + "!");
-                    System.out.println(
-                            "But because you are player one, player two will get a last turn to see if they can draw the game.");
+                    System.out.println("\nBut because you are player one, player two will get a last turn to see if they can draw the game.\n");
                     currentPlayer = p2;
-                    System.out.println(currentPlayer.getPlayerName() + " press enter to throw the dice a last time..");
+                    System.out.print(currentPlayer.getPlayerName() + " press enter to throw the dice a last time..");
                     String lastThrow = sc.nextLine();
                     while (!lastThrow.equals("")) {
                         // ask player to press enter
-                        System.out.println("input not understood.\nPress enter to throw dice");
+                        System.out.print("input not understood.\nPress enter to throw dice");
                         lastThrow = sc.nextLine();
                         continue;
                     }
@@ -98,13 +97,15 @@ public class Game {
 
                     if (!p2.checkPlayerBalance(WINNER_SCORE)) {
                         // Tell player one that they've won
-                        System.out.println("Congratulations " + p1.getPlayerName() + " you've won!\n"
+                        System.out.println("\nCongratulations " + p1.getPlayerName() + " you've won!\n"
                                 + p2.getPlayerName() + " loses the game with a balance of " + p2.getPlayerBalance());
+
                     } else {
                         // Tell that it is a draw
-                        System.out.println("The game is a draw!\n" + p2.getPlayerName()
-                                + " managed to reach the threshold of(WINNER_SCORE) on their last turn.");
+                        System.out.println("\nThe game is a draw!\n" + p2.getPlayerName()
+                                + " managed to reach the threshold of 3000 on their last turn.");
                     }
+                break;
                 }
                 // Tell the player they've won
                 System.out.println(currentPlayer.getPlayerName() + " congratulations, your balance is " + currentPlayer.getPlayerBalance() + "!\nYou've won!");
@@ -114,13 +115,6 @@ public class Game {
             System.out.println("Your balance is " + currentPlayer.getPlayerBalance() + "!");
             // check for extra turn
             if (board.getExtraTurn(diceCup.getTotalValue())) {
-                // Tell player they've gotten an extra turn
-                System.out.println(currentPlayer.getPlayerName() + " press enter to throw the dice");
-                if (!start.equals("")) {
-                    // ask player to press enter
-                    System.out.println("input not understood.\nPress enter to throw dice");
-                    continue;
-                }
                 continue;
             } else if (currentPlayer == p1) {
                 currentPlayer = p2;
