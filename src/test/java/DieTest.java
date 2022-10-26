3edfaf0123d74be970c3e143eb62a2a16f731e53
Mads Hauberg <77441913+hotbear1110@users.noreply.github.com>
2022-10-26 12:47:40 +0200

    Added comments and made TC3 into a loop that throws the die 1000 times

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -7,13 +7,25 @@ public class DieTest {
 
     private static Die die;
 
+    /**
+     * Before the tests are run, a new Die class is made.
+     */
     @BeforeClass
     public static void createDie() {
         die = new Die();
     }
+
+    /**
+     * TC3 test if the die will return a number that is unexpected (<1 or >6).
+     */
     @Test
     public void TC3() {
-        int dieValue = die.Throw();
-        assertTrue(1 <= dieValue && dieValue <= 6);
+        //A loop that throws the die 1000 times and checks if the value is as expected.
+        for (int i = 0; i < 1000; i++) {
+            //The die is thrown and a random number is returned.
+            int dieValue = die.Throw();
+            //Testing weather or not the dieValue is between 1 and 6.
+            assertTrue(1 <= dieValue && dieValue <= 6);
+        }
     }
 }
