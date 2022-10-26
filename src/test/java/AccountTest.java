315f6d4bebf8d4ebffb8084ffee304f4e1963ac7
fuckbrugernavne <113907244+fuckbrugernavne@users.noreply.github.com>
2022-10-26 13:57:52 +0200

    comments added on Accounttest

--- src/test/java/AccountTest.java
+++ src/test/java/AccountTest.java
@@ -5,18 +5,21 @@ import org.testng.annotations.Test;
 public class AccountTest {
     private Account account;
 
+    /*Before the test is run an account object is made, and it is tested for having the correct balance amount */
     @BeforeClass
     public void createAccount() {
         account = new Account();
         assertEquals(1000, account.getBalance());
     }
 
+    /*Tests that the addValue method understands that -500 means subtract */
     @Test
     public void TC1() {
         account.addValue(-500);
         assertEquals(500, account.getBalance());
     }
    
+    /*Tests that the balance can't be negative */
     @Test
     public void TC2(){
         account.addValue(-1001);
