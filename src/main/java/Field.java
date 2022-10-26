b5e05925a78639821d792f937fed2dfd779cabbc
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2022-10-26 13:17:56 +0200

    Lagt en ny note ind

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -12,10 +12,10 @@ public class Field {
     }
     // A constructor that makes an array of strings, integers and booleans. It also sets the name, effect and extra Turn.
     public String getFieldName(){return name;}
-
+    // Made it possible to get the fields name in the Board Class
     public Integer getFieldEffect(){return effect;}
-
+    // Made it possible to get the effect in the Board Class
     public Boolean getExtraTurn(){return extraTurn;}
-
+    // Made it possible to get the extra turn in the Board Class
 
 }
