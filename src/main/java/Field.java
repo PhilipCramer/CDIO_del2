c65331d3557003dece841f15b0f66786263cdec6
MathiasVestergaard1 <113906948+MathiasVestergaard1@users.noreply.github.com>
2022-10-26 13:02:34 +0200

    Addet konstrukteren og resten

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -1,2 +1,20 @@
 public class Field {
+
+    private final String name;
+    private final Integer effect;
+
+    private final Boolean extraTurn;
+
+    public Field(String name, Integer effect, Boolean extraTurn){
+        this.name = name;
+        this.effect = effect;
+        this.extraTurn = extraTurn;
+    }
+    public String getFieldName(String String){return name;}
+
+    public Integer getFieldEffect(Integer Int){return effect;}
+
+    public Boolean getExtraTurn(){return extraTurn;}
+
+
 }
